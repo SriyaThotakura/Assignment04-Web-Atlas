675b566bba468ebced910fe6b4dcf881a5188170
SriyaThotakura <sriyagayathri007@gmail.com>
2025-11-07 08:38:43 -0500

    assignment04

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,3 +1,24 @@
 // import { defineCollection, z } from "astro:content";
+type Site = {
+  NAME: string;
+  EMAIL: string;
+  DESCRIPTION: string;
+};
+
+type Metadata = {
+  TITLE: string;
+  DESCRIPTION: string;
+};
 
 export const collections = {};
+
+export const SITE: Site = {
+  NAME: "Sensing the Inequity",
+  EMAIL: "sriyathotakura@example.com",
+  DESCRIPTION: "An atlas of environmental injustice in the South Bronx.",
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Sensing the Inequity Atlas Homepage",
+};
